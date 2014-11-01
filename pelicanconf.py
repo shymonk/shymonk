22aad162fd4c50d72f51ce3596fd6eaa0b74b2d4
shymonk <hellojohn201@gmail.com>
2014-11-02 00:12:39 +0800

    Refactoring pelicanconf

--- pelicanconf.py
+++ pelicanconf.py
@@ -6,18 +6,46 @@ AUTHOR = 'shymonk'
 SITENAME = "Shymonk Blog"
 SITEURL = ''
 
+# Article path
 PATH = 'posts'
+
+# Theme
 THEME = 'themes/frankies'
+
+# Locale
+DEFAULT_LANG = 'en'
+LOCALE = ('en_US',)
 TIMEZONE = 'Asia/Shanghai'
 
-DEFAULT_LANG = 'zh'
+# Datetime
+DEFAULT_DATE = 'fs'
+DEFAULT_DATE_FORMAT = '%B %d, %Y'
+
+# Place articles in a location such as {slug}/index.html
+# and link to them as {slug} instead of {slug}.html
+# for clean URLs
+ARTICLE_URL = "posts/{date:%Y}/{date:%m}/{slug}/"
+ARTICLE_SAVE_AS = "posts/{date:%Y}/{date:%m}/{slug}/index.html"
+CATEGORY_URL = "category/{slug}"
+CATEGORY_SAVE_AS = "category/{slug}/index.html"
+TAG_URL = "tag/{slug}/"
+TAG_SAVE_AS = "tag/{slug}/index.html"
+YEAR_ARCHIVE_SAVE_AS = 'posts/{date:%Y}/index.html'
+
+# Disqus
+DISQUS_SITENAME = "shymonk"
 
 # Feed generation is usually not desired when developing
 FEED_ALL_ATOM = None
 CATEGORY_FEED_ATOM = None
 TRANSLATION_FEED_ATOM = None
 
-# Blogroll
+MENUITEMS = (
+    ('Home', '/'),
+    ('Archives', '/archives.html'),
+    ('Projects', '/projects.html'),
+)
+
 LINKS = (('Pelican', 'http://getpelican.com/'),
          ('Python.org', 'http://python.org/'),
          ('Jinja2', 'http://jinja.pocoo.org/'),
