1b4316aac981a8919359582c1b81df171f4b5391
shymonk <hellojohn201@gmail.com>
2017-04-22 15:28:27 +0800

    User relative url

--- publishconf.py
+++ publishconf.py
@@ -10,7 +10,7 @@ import sys
 sys.path.append(os.curdir)
 from pelicanconf import *
 
-SITEURL = 'http://shymonk.com'
+SITEURL = '//shymonk.com'
 RELATIVE_URLS = False
 
 FEED_ALL_ATOM = 'feeds/all.atom.xml'
